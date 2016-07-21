62e2c0d638a7d7404e7b223759230688cc889b3f
Kenneth Lee <kennyxlee@gmail.com>
2016-07-21 14:28:24 -0700

    Add 'value' prop to Board component

--- app/Board.js
+++ app/Board.js
@@ -11,11 +11,12 @@ var Board = React.createClass({
     return (
       <div style={styles.board}>
         {this.props.board.map((element, rows) => {
-          return element.map((square, cols) => {
+          return element.map((value, cols) => {
             if (cols === 2) {
               return  <span>
                         <Square makeMove={this.props.makeMove}
                                 rows={rows}
+                                value={value}
                                 cols={cols}
                                  />
                                <br />
@@ -23,6 +24,7 @@ var Board = React.createClass({
             } else {
               return <Square  makeMove={this.props.makeMove}
                               rows={rows}
+                              value={value}
                               cols={cols} />
             }
           })
