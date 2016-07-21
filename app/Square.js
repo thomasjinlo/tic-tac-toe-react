1439e7ceedae2454ec55c7a56ff158a1167fba9f
Kenneth Lee <kennyxlee@gmail.com>
2016-07-21 14:31:43 -0700

    Add background color for Square component

--- app/Square.js
+++ app/Square.js
@@ -1,12 +1,22 @@
 var React = require('react');
 var styles = require('./styles');
 
+var color = function(value) {
+  if (value === 'X') {
+    return styles.ex;
+  } else if (value === 'O') {
+    return styles.oh;
+  } else {
+    return;
+  }
+}
+
 var Square = React.createClass({
 
   render() {
     return (
-      <div  onClick={this.props.makeMove.bind(this, this.props.rows, this.props.cols)}
-            style={styles.square}></div>
+      <span  onClick={this.props.makeMove.bind(this, this.props.rows, this.props.cols)}
+            style={Object.assign({}, styles.square, color(this.props.value))}>{this.props.value}</span>
     )
   }
 })
--- app/styles.js
+++ app/styles.js
@@ -1,9 +1,15 @@
 var styles = {
   square: {
-    width: '100px',
-    height: '100px',
+    minWidth: '100px',
+    minHeight: '100px',
     border: '1px solid black',
     display: 'inline-block'
+  },
+  ex: {
+    backgroundColor: 'black',
+  },
+  oh: {
+    backgroundColor: 'red',
   },
   board: {
     width: 'auto',
